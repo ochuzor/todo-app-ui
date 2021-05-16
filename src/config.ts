63d6a90637599d01464a818f35fedc1d36d2b549
Chinedu Eze <ochuzor@gmail.com>
2021-05-16 19:15:09 +0300

    fix the env variable for API_URL

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const TOKEN_KEY = 'TOKEN_KEY';
-export const API_URL = process.env.API_URL || 'http://127.0.0.1:8000';
+export const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
